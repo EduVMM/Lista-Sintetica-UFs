d85cbc0d8176caf27906b9aafa8ed4e1e2d28aa7
EduVMM <90155244+EduVMM@users.noreply.github.com>
2023-06-14 10:24:15 +0000

    Adicionando estados

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista Sintética de UFs</title>
+    <style>
+        table{
+          border-collapse: collapse;
+          text-align: center;
+        }
+    </style>
+</head>
+<body>
+<?php
+$uf = [
+    'Acre' => [
+        'Bandeira' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Bandeira_do_Acre.svg/45px-Bandeira_do_Acre.svg.png',
+        'Unidade Federativa' => 'Acre',
+        'Abreviação' => 'AC',
+        'Sede do Governo' => 'Rio Branco',
+        'Área' => '164 122,2',
+        'População' => '795 145',
+        'Densidade' => '4,30',
+        'Pib (2015)' => '13 622 000',
+        'Pib Percentual Total (2015)' => '0,2',
+        'Pib per capita' => '16 953,46',
+        'IDH (2010)' => '0,663',
+        'Alfabetização (2016)' => '86,9%',
+        'Mortalidade Infantil (2016)' => '17,0‰',
+        'Expectativa de Vida (2016)' => '73,9 anos'
+    ]
+]
+?>
